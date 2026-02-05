4674a1c5375eaa67e68c3e67e8529b9952528f5c
vijay_p <vijayputtarevaiah@gmail.com>
2026-02-05 14:48:54 -0400

    Added main method

---
+++ src/main/java/com/example/advancesoftwarequiz3/Advancesoftwarequiz3Application.java
@@ -0,0 +1,13 @@
+package com.example.advancesoftwarequiz3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Advancesoftwarequiz3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Advancesoftwarequiz3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advancesoftwarequiz3/Advancesoftwarequiz3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancesoftwarequiz3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Advancesoftwarequiz3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
